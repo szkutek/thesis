c5f974a98c63ac3c0f33eaf540b9c5cf77a6529d
szkutek <aga.szkutek@gmail.com>
2018-04-25 15:03:13 +0200

    create graph for gminas (weights)

--- code/create_graphs.py
+++ code/create_graphs.py
@@ -4,22 +4,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ == '__main__':
-    type = 'voivodeships'
+    type = 'gminas'
     with open('../data/' + type + '_neighbours.json') as file:
         nbrs = json.load(file)  # dict {gminas teryt: list of nbrs teryts}
-        g = nx.from_dict_of_lists(nbrs)
+        G = nx.from_dict_of_lists(nbrs)
 
-        # work_migration = pd.read_csv('../data/l_macierz_2014_03_18.csv', delimiter=';',
-        #                              dtype={0: str, 1: str, 2: str})
-        # work_migration = work_migration.head(10)
-        # work_migration = work_migration.loc[:, work_migration.columns[:3]]
-        # edges = []
-        # work_migration.apply(lambda x: edges.append((x[0], x[1], x[2])), axis=1)
-        # g.add_edges_from(edges)
-
-        edges = [('02', '04', '20'), ('06', '08', '50')]
+        work_migration = pd.read_csv('../data/l_macierz_2014_03_18.csv', delimiter=';',
+                                     dtype={0: str, 1: str})
+        work_migration = work_migration.loc[:, work_migration.columns[:3]]
+        edges = []
+        work_migration.apply(lambda x: edges.append((x[0], x[1], x[2])), axis=1)
+        # TODO add weights to edges
+        # TODO if edge doesn't exits -> find shortest path and accumulate weights
+        edges = {(e1, e2): w for e1, e2, w in edges}
         print(edges)
-        nx.draw(g)
-        # nx.draw_networkx_edge_labels(g)
 
-        plt.show()
+        # edges = {('02', '04'): '20', ('06', '08'): '50'}
+        # G.add_edges_from(edges)
+        # pos = nx.spring_layout(G)
+        #
+        # nx.draw(G)
+        # nx.draw_networkx_edge_labels(G, pos, edge_labels=edges)
+        #
+        # plt.savefig('graph_' + type + '.png')
